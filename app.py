2d64f0cf2911b559758c8291844fca33dad342a1
Keerthana Muralidharan <keerthanam@microsoft.com>
2023-03-23 12:43:54 +0000

    integration changes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from db_connector import DBConnector
+import os
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 db_obj = DBConnector()
 
@@ -11,20 +14,25 @@ def hello():
     return 'Hello, World!'
 
 
-@app.route('/processLog', methods=['POST', 'PUT'])
+@app.route('/processLog', methods=['POST'])
 def process_log():
-    data = request.args.to_dict()
-    data['status'] = 'init'
+    productName = request.form['productName']
+    logFileName = request.form['logFileName']
+    error_file = request.files['stacktraceFile']
+    data = {'productName':productName, 'logFileName':logFileName, 'state':'init'}
     id = db_obj.db_insert('bug_info', data)
-    response = jsonify({'status': 'Submitted log data for processing!', 'request id': id})
+    filename = id + '.' + '.'.join(error_file.filename.split('.')[1:])
+    error_file.save(os.path.join('/logs_to_be_processed', filename))
+    response = jsonify({'status': 'Submitted log data for processing!', 'requestId': id})
     return response
 
 
 @app.route('/processLog/status',  methods=['GET'])
 def get_status():
     request_id = request.args.get('requestId')
-    status = db_obj.db_fetch_status_for_id('bug_info', request_id)
-    response = jsonify({'requestId': request_id, 'stauts': status})
+    print("get status info for request - ", request_id)
+    state = db_obj.db_fetch_state_for_id('bug_info', request_id)
+    response = jsonify({'requestId': request_id, 'state': state})
     return response
 
 
--- db_connector.py
+++ db_connector.py
@@ -8,18 +8,15 @@ class DBConnector:
         self.db = client['mwizz_db']
 
     def db_insert(self, collection, data):
-        print(data)
         result = self.db[collection].insert_one(data)
         return str(result.inserted_id)
     
-    def db_fetch_status_for_id(self, collection, id):
+    def db_fetch_state_for_id(self, collection, id):
         document_id = ObjectId(id)  
         data = self.db[collection].find_one({"_id": document_id})
-        print(data)
-        return (data['status'])
+        return (data['state'])
 
     def db_fetch_all_request_details(self, collection, product_name):
-        print(product_name)
         data = list(self.db[collection].find({"productName": product_name}))
         for item in data:
             item["_id"] = str(item["_id"])
